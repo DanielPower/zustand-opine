4809f9aad4e41d83232cfc02f5d0327adc51869e
Daniel Power <me@danielpower.ca>
2022-01-13 01:37:48 -0330

    Move actions out of store to api.actions. Auto-generate selectors

--- index.js
+++ index.js
@@ -5,20 +5,6 @@ import produce from "immer";
 const createAction = (name, action, set, get, api) =>
   action((prev, replace) => set(prev, replace, name), get, api);
 
-export const createSlice =
-  (sliceName, { actions, initialState }) =>
-  (set, get, api) => ({
-    [sliceName]: {
-      ...Object.fromEntries(
-        Object.entries(actions).map(([actionName, action]) => [
-          actionName,
-          createAction(`${sliceName}/${actionName}`, action, set, get, api),
-        ])
-      ),
-      ...initialState,
-    },
-  });
-
 const immer = (config) => (set, get, api) =>
   config(
     (partial, replace) => {
@@ -30,19 +16,42 @@ const immer = (config) => (set, get, api) =>
     api
   );
 
-const createStore = (slices) =>
-  zustand(
-    immer(
-      devtools((set, get, api) =>
-        slices.reduce(
-          (state, slice) => ({
-            ...state,
-            ...slice(set, get, api),
-          }),
-          {}
-        )
-      )
-    )
+function createSelectors(store) {
+  store.use = {};
+  Object.keys(store.getState()).forEach((key) => {
+    const selector = (state) => state[key];
+    store.use[key] = () => store(selector);
+  });
+  return store;
+}
+
+const slicedState = (slices) => (set, get, api) => {
+  api.actions = {};
+  const initialState = {};
+  Object.entries(slices).forEach(([sliceKey, slice]) => {
+    Object.entries(slice.actions).forEach(([actionKey, action]) => {
+      if (typeof api.actions[actionKey] !== "undefined") {
+        throw new Error(`Duplicate action with name '${name}' created`);
+      }
+      api.actions[actionKey] = createAction(actionKey, action, set, get, api);
+      initialState[sliceKey] = slice.initialState;
+    });
+  });
+  return initialState;
+};
+
+const createStore = (slices, devtoolsOptions = {}) => {
+  const actualDevtoolsOptions = {
+    ...devtoolsOptions,
+    features: {
+      ...devtoolsOptions?.features,
+      test: false,
+    },
+  };
+
+  return createSelectors(
+    zustand(immer(devtools(slicedState(slices), actualDevtoolsOptions)))
   );
+};
 
 export default createStore;
