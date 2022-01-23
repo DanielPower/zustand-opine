c77f041cc3f86f09b24cfef8d9c060ec4039fda6
Daniel Power <me@danielpower.ca>
2022-01-23 04:03:46 -0330

    Restore actions

--- src/index.ts
+++ src/index.ts
@@ -3,37 +3,71 @@ import { devtools, NamedSet } from "zustand/middleware";
 
 interface Slice {
   initialState: Record<string, any>;
+  actions?: Record<string, any>;
 }
 
-export type TransformedStore<TStore extends object> = {
+export type OpineState<TStore extends object> = {
   [K in keyof TStore]: TStore[K] extends { initialState: infer U } ? U : never;
 };
+
+export interface OpineApi<TStore extends object> extends StoreApi<TStore> {
+  actions: Record<string, Record<string, Function>>;
+}
+
+const createAction = <TStore extends Record<string, any>>(
+  name: string,
+  action: Function,
+  set: NamedSet<TStore>,
+  get: GetState<TStore>,
+  api: StoreApi<TStore>
+) =>
+  action(
+    (prev: TStore, replace: boolean) => set(prev, replace, name),
+    get,
+    api
+  );
 
 const structuredStore =
   <TStore extends Record<string, Slice>>(slices: Record<string, Slice>) =>
   (
     set: NamedSet<TStore>,
     get: GetState<TStore>,
-    api: StoreApi<TStore>
-  ): TransformedStore<TStore> =>
-    Object.fromEntries(
-      Object.entries(slices).map(([sliceKey, slice]) => [
-        sliceKey,
-        slice.initialState,
-      ])
-    ) as TransformedStore<TStore>;
+    api: OpineApi<TStore>
+  ): OpineState<TStore> => {
+    api.actions = {};
+    const state = {};
+    Object.entries(slices).forEach(([sliceKey, slice]) => {
+      api.actions[sliceKey] = {};
+      Object.entries(slice.actions).forEach(([actionKey, action]) => {
+        api.actions[sliceKey][actionKey] = createAction(
+          `${sliceKey}/${actionKey}`,
+          action,
+          set,
+          get,
+          api
+        );
+      });
+      state[sliceKey] = slice.initialState;
+    });
+    return state as OpineState<TStore>;
+  };
 
 const createStore = <TStore extends Record<string, any>>(
   slices: Record<string, Slice>,
-  devtoolsOptions = {}
+  devtoolsOptions = {} // TODO add typing
 ) => {
-  type ActualStore = TransformedStore<TStore>;
-  return zustand<ActualStore>(
+  type ActualStore = OpineState<TStore>;
+  return zustand<
+    ActualStore,
+    SetState<ActualStore>,
+    GetState<ActualStore>,
+    OpineApi<ActualStore>
+  >(
     devtools<
       ActualStore,
       SetState<ActualStore>,
       GetState<ActualStore>,
-      StoreApi<ActualStore>
+      OpineApi<ActualStore>
     >(structuredStore<TStore>(slices), devtoolsOptions)
   );
 };
