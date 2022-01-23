dd17eeeaa60877e32877c515a9329cbd610ca636
Daniel Power <me@danielpower.ca>
2022-01-23 04:51:31 -0330

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	},
+	plugins: ["@typescript-eslint"],
+	rules: {
+		"linebreak-style": ["error", "unix"],
+		quotes: ["error", "double"],
+		semi: ["error", "always"],
+	},
+};
