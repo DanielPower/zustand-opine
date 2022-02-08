197ba83c2680553fdd2f69b98d95a9aab204b77b
Daniel Power <me@danielpower.ca>
2022-02-08 17:48:03 -0330

    Add custom selectors to the store

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ import zustand from "zustand";
 import { devtools } from "zustand/middleware";
 import immer from "./middleware/immer";
 
+const createSelectors = (store) => {
+	store.use = {};
+	Object.keys(store.getState()).forEach((sliceKey) => {
+		store.use[sliceKey] = {};
+		store.use[sliceKey].all = () => store((state) => state[sliceKey]);
+		Object.entries(store.selectors[sliceKey]).forEach(
+			([selectorKey, selector]) => {
+				store.use[sliceKey][selectorKey] = () => store(selector);
+			}
+		);
+	});
+	return store;
+};
+
 const parseSlices = (slices) => {
 	const initialState = {};
 	const actions = {};
+	const selectors = {};
 	Object.entries(slices).forEach(([key, slice]) => {
 		initialState[key] = slice.initialState;
 		actions[key] = slice.actions;
+		selectors[key] = slice.selectors;
 	});
-	return { initialState, actions };
+	return { initialState, actions, selectors };
 };
 
 const apiActions = (config, actionSlices) => {
@@ -29,9 +45,33 @@ const apiActions = (config, actionSlices) => {
 	};
 };
 
+const apiSelectors = (config, selectorSlices) => {
+	return (set, get, api) => {
+		api.selectors = {};
+		Object.entries(selectorSlices).forEach(([sliceKey, selectors]) => {
+			api.selectors[sliceKey] = {};
+			Object.entries(selectors).forEach(([selectorKey, selector]) => {
+				api.selectors[sliceKey][selectorKey] = selector;
+			});
+		});
+		return config(set, get, api);
+	};
+};
+
 const createStore = (slices) => {
-	const { initialState, actions } = parseSlices(slices);
-	return zustand(immer(devtools(apiActions(() => initialState, actions))));
+	const { initialState, actions, selectors } = parseSlices(slices);
+	return createSelectors(
+		zustand(
+			immer(
+				devtools(
+					apiActions(
+						apiSelectors(() => initialState, selectors),
+						actions
+					)
+				)
+			)
+		)
+	);
 };
 
 export default createStore;
